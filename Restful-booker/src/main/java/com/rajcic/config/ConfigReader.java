084e9bd84bcc089bf8d465e64794116ac02c6923
Djordje Rajcic <84-2022@pmf.kg.ac.rs>
2026-07-06 18:27:53 +0200

    Yaml script and few changes

--- Restful-booker/src/main/java/com/rajcic/config/ConfigReader.java
+++ Restful-booker/src/main/java/com/rajcic/config/ConfigReader.java
@@ -1,24 +1,25 @@
 package com.rajcic.config;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
     private static Properties properties = new Properties();
 
-    static {
-
-        try {
-            FileInputStream file = new FileInputStream("src/main/resources/config.properties");
-
-            properties.load(file);
-        } catch (Exception e) {
+        static {
+            try {
+                FileInputStream file = new FileInputStream("src/main/resources/config.properties");
+                properties.load(file);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
     }
 
         public static String get(String key){
 
+            String envValue = System.getenv(key.toUpperCase());
+            if(envValue !=null) return envValue;
             return properties.getProperty(key);
         }
     }
